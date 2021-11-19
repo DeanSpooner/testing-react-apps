aae916eb63ae271bd5ca5722b360303fdd94016c
Dean Spooner <dean.spooner@deptagency.com>
2021-11-19 16:19:07 +0000

    Exercises completed to end of 07.

--- src/__tests__/exercise/07-extra1.js
+++ src/__tests__/exercise/07-extra1.js
@@ -7,8 +7,6 @@ import {ThemeProvider} from '../../components/theme';
 import EasyButton from '../../components/easy-button';
 
 test('renders with the light styles for the light theme', () => {
-  // 🐨 uncomment all of this code and your test will be busted on the next line:
-
   const Wrapper = ({children}) => (
     <ThemeProvider initialTheme="light">{children}</ThemeProvider>
   );
@@ -19,9 +17,19 @@ test('renders with the light styles for the light theme', () => {
     background-color: white;
     color: black;
   `);
-  //
-  // 🐨 update the `render` call above to use the wrapper option using the
-  // ThemeProvider
+});
+
+test('renders with the dark styles for the dark theme', () => {
+  const Wrapper = ({children}) => (
+    <ThemeProvider initialTheme="dark">{children}</ThemeProvider>
+  );
+
+  render(<EasyButton>Easy</EasyButton>, {wrapper: Wrapper});
+  const button = screen.getByRole('button', {name: /easy/i});
+  expect(button).toHaveStyle(`
+    background-color: black;
+    color: white;
+  `);
 });
 
 /* eslint no-unused-vars:0 */
