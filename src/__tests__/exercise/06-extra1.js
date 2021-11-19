9492e25866350b5a6f6fb41e2d514389ae91fa35
Dean Spooner <dean.spooner@deptagency.com>
2021-11-19 15:19:10 +0000

    Exercises completed to end of 06.

---
+++ src/__tests__/exercise/06-extra1.js
@@ -0,0 +1,48 @@
+// mocking Browser APIs and modules
+// http://localhost:3000/location
+
+import * as React from 'react';
+import {render, screen, act} from '@testing-library/react';
+import {useCurrentPosition} from 'react-use-geolocation';
+import Location from '../../examples/location';
+
+jest.mock('react-use-geolocation');
+
+test('displays the users current location', () => {
+  const fakePosition = {
+    coords: {
+      latitude: 53,
+      longitude: -2,
+    },
+  };
+
+  let setReturnValue;
+  const useMockCurrentPosition = () => {
+    const state = React.useState([]);
+    setReturnValue = state[1];
+    return state[0];
+  };
+  useCurrentPosition.mockImplementation(useMockCurrentPosition);
+
+  render(<Location />);
+  expect(screen.getByLabelText(/loading/i)).toBeInTheDocument();
+
+  act(() => {
+    setReturnValue([fakePosition]);
+  });
+
+  expect(screen.queryByLabelText(/loading/i)).not.toBeInTheDocument();
+
+  expect(screen.getByText(/latitude/i)).toHaveTextContent(
+    `Latitude: ${fakePosition.coords.latitude}`,
+  );
+
+  expect(screen.getByText(/longitude/i)).toHaveTextContent(
+    `Longitude: ${fakePosition.coords.longitude}`,
+  );
+});
+
+/*
+eslint
+  no-unused-vars: "off",
+*/
